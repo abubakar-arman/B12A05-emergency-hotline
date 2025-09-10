3b5169cc7d8b93569bd71095d8a9b4705e2692b6
abubakar-arman <abubakar.arman.cse@gmail.com>
2025-09-10 22:22:20 +0600

    Add Event Delegation on click event

--- js/index.js
+++ js/index.js
@@ -29,22 +29,36 @@ const addToHistory = function(serviceName, phone){
 //     return modal
 // }
 
-// Implement Heart Count Feature
-const btnHearts = document.getElementsByClassName('btn-heart')
-
-for (const btnHeart of btnHearts){
-    btnHeart.addEventListener('click', function(){
+// Event Delegation on Click event
+document.addEventListener('click', async function(e){
+    // handle click on heart button
+    const btnHeart = e.target.closest('.btn-heart')
+    if(btnHeart){
         const val = getTextInt('heart-count')
         setText('heart-count', val+1)
-    })
-}
+        return
+    }
 
-// Implement Calling and Call History Feature
-const btnCalls = document.getElementsByClassName('btn-call')
-for (const btnCall of btnCalls){
-    btnCall.addEventListener('click', function(e){
-        const serviceName = e.target.parentNode.parentNode.querySelector('h4:nth-child(2)').innerText
-        const phone = e.target.parentNode.parentNode.querySelector('h4:nth-child(4)').innerText
+    // handle clear history button
+    const btnClearHistory = e.target.closest('#btn-clear-history')
+    console.log(btnClearHistory,333)
+    if(btnClearHistory){
+        const historyList = document.getElementById('history-list')
+        if(historyList.childNodes.length){
+            historyList.innerText = ''
+        } else {
+            alert('Empty List!\nThere is nothing to be cleared')
+            // showModal('Empty List', 'There is nothing to be cleared')
+        }
+        return
+    }
+
+    // handle call button click event
+    const btnCall = e.target.closest('.btn-call')
+    if(btnCall){
+        const card = e.target.closest('#card')
+        const serviceName = card ? card.querySelector('#service-name').innerText : null
+        const phone = card ? card.querySelector('#phone').innerText : null
         console.log(serviceName, phone)
         
         const coin = getTextInt('coin-count')
@@ -55,45 +69,42 @@ for (const btnCall of btnCalls){
         }
         setText('coin-count', coin-20)
         console.log(coin)
-
+        
         alert(`Calling ${serviceName} : ${phone}`)
         // showModal(serviceName, 'Contact : '+phone)
         addToHistory(serviceName, phone)
-    })
-}
+        return
+    }
 
-document.getElementById('btn-clear-history').addEventListener('click', function(){
-    const historyList = document.getElementById('history-list')
-    if(historyList.childNodes.length){
-        historyList.innerText = ''
-    } else {
-        alert('Empty List!\nThere is nothing to be cleared')
-        // showModal('Empty List', 'There is nothing to be cleared')
+    // handle copy button click
+    const btnCopy = e.target.closest('.btn-copy')
+    if(btnCopy){
+        const copyCount = getTextInt('copy-count')
+        setText('copy-count', copyCount+1)
+        
+        // const phone = btnCopy.parentNode.parentNode.querySelector('h4:nth-child(4)').innerText
+        const phone = btnCopy.getAttribute('data-copy-source')
+        console.log('copied number to clipboard :', phone)
+        
+        try {
+            await navigator.clipboard.writeText(phone)
+        } catch(err){
+            console.log(err)
+        }
+        
+        alert('Clipboard\nContact has been copied')
+        // const modal = showModal('Clipboard', 'Contact has been copied')
+        // setTimeout(() => {
+        //     modal.close()
+        // }, 1000)
     }
 })
+
 
 // Implement Contact Copy
 const btnCopies = document.getElementsByClassName('btn-copy')
 // console.log(btnCopies)
 for (const btnCopy of btnCopies){
     btnCopy.addEventListener('click', async function(e){
-        const copyCount = getTextInt('copy-count')
-        setText('copy-count', copyCount+1)
-
-        // const phone = btnCopy.parentNode.parentNode.querySelector('h4:nth-child(4)').innerText
-        const phone = btnCopy.getAttribute('data-copy-source')
-        console.log('copied number to clipboard :', phone)
-
-        try {
-            await navigator.clipboard.writeText(phone)
-        } catch(err){
-            console.log(err)
-        }
-
-        alert('Clipboard\nContact has been copied')
-        // const modal = showModal('Clipboard', 'Contact has been copied')
-        // setTimeout(() => {
-        //     modal.close()
-        // }, 1000)
     })
 }
